f41f4e391cd21feb9c3d5a6e84ddbcc064d502a9
Hendi Santika <hendisantika@yahoo.co.id>
2022-05-17 09:01:22 +0700

    #33 - Add AppUserDetailsService

--- src/main/java/com/hendisantika/service/AppUserDetailsService.java
+++ src/main/java/com/hendisantika/service/AppUserDetailsService.java
@@ -1,9 +1,17 @@
 package com.hendisantika.service;
 
+import com.hendisantika.domain.User;
 import com.hendisantika.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,4 +28,22 @@ import org.springframework.stereotype.Component;
 public class AppUserDetailsService implements UserDetailsService {
     private UserRepository userRepository;
 
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(s);
+
+        if (user == null) {
+            throw new UsernameNotFoundException(String.format("The username %s doesn't exist", s));
+        }
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        user.getRoles().forEach(role -> {
+            authorities.add(new SimpleGrantedAuthority(role.getRoleName()));
+        });
+
+        UserDetails userDetails = new org.springframework.security.core.userdetails.
+                User(user.getUsername(), user.getPassword(), authorities);
+
+        return userDetails;
+    }
 }
