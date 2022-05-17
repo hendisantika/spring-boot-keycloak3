603390ae27d3581f2db89f9541b3296a1a02bb1c
Hendi Santika <hendisantika@yahoo.co.id>
2022-05-17 08:52:24 +0700

    #15 - Add authenticationManager in SecurityConfig

--- src/main/java/com/hendisantika/config/SecurityConfig.java
+++ src/main/java/com/hendisantika/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.hendisantika.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -29,4 +31,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Value("${security.security-realm}")
     private String securityRealm;
+
+    @Bean
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
 }
