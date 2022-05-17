cef5da0c7087d82e25784fb01f4d50e627838cc0
Hendi Santika <hendisantika@yahoo.co.id>
2022-05-17 08:47:36 +0700

    #5 - Add AuthorizationServerConfig

---
+++ src/main/java/com/hendisantika/config/AuthorizationServerConfig.java
@@ -0,0 +1,56 @@
+package com.hendisantika.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-keycloak3
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 17/05/22
+ * Time: 08.46
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Value("${security.jwt.client-id}")
+    private String clientId;
+
+    @Value("${security.jwt.client-secret}")
+    private String clientSecret;
+
+    @Value("${security.jwt.grant-type}")
+    private String grantType;
+
+    @Value("${security.jwt.scope-read}")
+    private String scopeRead;
+
+    @Value("${security.jwt.scope-write}")
+    private String scopeWrite = "write";
+
+    @Value("${security.jwt.resource-ids}")
+    private String resourceIds;
+
+    @Autowired
+    private TokenStore tokenStore;
+
+    @Autowired
+    private JwtAccessTokenConverter accessTokenConverter;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+}
