bd9453405458df7db0af2467acbd5aab050b3efc
Hendi Santika <hendisantika@yahoo.co.id>
2022-05-17 08:50:55 +0700

    #12 - Add ResourceServerConfig Bean in DatasourceConfig

---
+++ src/main/java/com/hendisantika/config/ResourceServerConfig.java
@@ -0,0 +1,29 @@
+package com.hendisantika.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-keycloak3
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 17/05/22
+ * Time: 08.50
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+    @Autowired
+    private ResourceServerTokenServices tokenServices;
+
+    @Value("${security.jwt.resource-ids}")
+    private String resourceIds;
+
+}
