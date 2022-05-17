63e978da63735aa12375c85ce6460faa2df4d3ab
Hendi Santika <hendisantika@yahoo.co.id>
2022-05-17 08:55:23 +0700

    #22 - Add Role Domain

---
+++ src/main/java/com/hendisantika/domain/Role.java
@@ -0,0 +1,38 @@
+package com.hendisantika.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-keycloak3
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 17/05/22
+ * Time: 08.54
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(name = "app_role")
+@Getter
+@Setter
+public class Role {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "role_name")
+    private String roleName;
+
+    @Column(name = "description")
+    private String description;
+}
