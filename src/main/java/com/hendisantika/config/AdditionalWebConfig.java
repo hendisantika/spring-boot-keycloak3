37514229b160d5137e272be07aa7f0446f5342ba
Hendi Santika <hendisantika@yahoo.co.id>
2022-05-17 08:46:43 +0700

    #4 - Add AdditionalWebConfig

---
+++ src/main/java/com/hendisantika/config/AdditionalWebConfig.java
@@ -0,0 +1,40 @@
+package com.hendisantika.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-keycloak3
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 17/05/22
+ * Time: 08.46
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class AdditionalWebConfig {
+    /**
+     * Allowing all origins, headers and methods here is only intended to keep this example simple.
+     * This is not a default recommended configuration. Make adjustments as
+     * necessary to your use case.
+     */
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
+    }
+}
