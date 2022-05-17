f5e3ca32c4ccc1a6b02b46ecb23224cab1ad2f01
Hendi Santika <hendisantika@yahoo.co.id>
2022-05-17 08:54:43 +0700

    #21 - Add RandomCity Domain

---
+++ src/main/java/com/hendisantika/domain/RandomCity.java
@@ -0,0 +1,35 @@
+package com.hendisantika.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-keycloak3
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 17/05/22
+ * Time: 08.54
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(name = "random_city")
+@Getter
+@Setter
+public class RandomCity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+}
