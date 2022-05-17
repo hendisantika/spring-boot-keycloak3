cac689078742e2f2182a595cd6c8fa4066f9c345
Hendi Santika <hendisantika@yahoo.co.id>
2022-05-17 08:35:30 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/SpringBootKeycloak3Application.java
@@ -0,0 +1,13 @@
+package com.hendisantika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKeycloak3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootKeycloak3Application.class, args);
+    }
+
+}
