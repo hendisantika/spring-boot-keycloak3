27df46841adee19a00802e4c82e5d683bbe1279b
Hendi Santika <hendisantika@yahoo.co.id>
2022-05-17 08:57:23 +0700

    #24 - Add RandomCityRepository

---
+++ src/main/java/com/hendisantika/repository/RandomCityRepository.java
@@ -0,0 +1,17 @@
+package com.hendisantika.repository;
+
+import com.hendisantika.domain.RandomCity;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-keycloak3
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 17/05/22
+ * Time: 08.57
+ * To change this template use File | Settings | File Templates.
+ */
+public interface RandomCityRepository extends CrudRepository<RandomCity, Long> {
+}
